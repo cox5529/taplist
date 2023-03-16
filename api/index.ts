649dbcf5e68ce592cd8c9c557496f2ef4a61d4ad
Brandon Cox <brandon.cox@metova.com>
2023-03-15 21:27:17 -0500

    Reduce db reads and writes

--- api/index.ts
+++ api/index.ts
@@ -2,6 +2,8 @@ import { applicationDefault, initializeApp } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 import net from 'net';
 
+import { Scale } from './../src/models/scale';
+
 enum MessageTypes {
   Ping = 6,
   Hardware = 20,
@@ -15,11 +17,15 @@ type Message = {
   bodyText?: string[];
 };
 
+const kegs: { [key: string]: Scale } = {};
+
 const app = initializeApp({
   credential: applicationDefault(),
 });
 
 const db = getFirestore(app);
+
+db.getAll().then((x) => x.forEach((item) => (kegs[item.id] = item.data() as Scale)));
 
 const parseToStringArray = (body: Buffer): string[] => {
   let s = '';
@@ -42,12 +48,10 @@ const parseToStringArray = (body: Buffer): string[] => {
 };
 
 const handleHardwareData = async (ip: string, pin: string, value: string): Promise<void> => {
-  let keg = await db
-    .doc(`scales/${ip}`)
-    .get()
-    .then((x) => x.data());
+  let keg = kegs[ip];
   if (!keg) {
     keg = {
+      ip,
       isPouring: false,
       lastPour: 0,
       ouncesRemaining: 0,
@@ -57,11 +61,17 @@ const handleHardwareData = async (ip: string, pin: string, value: string): Promi
   }
 
   if (pin === '51') {
-    keg.ouncesRemaining = parseFloat(value) * 33.814;
+    const newOunces = parseFloat(value) * 33.814;
     keg.percentFull = (keg.ouncesRemaining / keg.totalVolume) * 100;
-  }
 
-  db.doc(`scales/${ip}`).set(keg);
+    if (Math.abs(newOunces - keg.ouncesRemaining) < 0.5) {
+      return;
+    }
+
+    keg.ouncesRemaining = newOunces;
+
+    db.doc(`scales/${ip}`).set(keg);
+  }
 };
 
 const parse = (data: Buffer): Message => {
