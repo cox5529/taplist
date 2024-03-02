4639df443d82674ce3f6c0da1c7a563fea5cb108
Brandon Cox <brandon.cox@metova.com>
2024-03-02 14:47:23 -0500

    Only show beers currently on tap

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -25,19 +25,29 @@ export const syncFromBrewfather = functions
       ibu: x.estimatedIbu,
       originalGravity: x.measuredOg ?? x.estimatedOg,
       finalGravity: x.measuredFg ?? x.estimatedFg,
-      name: x.recipe.name,
+      name: `${x.recipe.name} (Batch #${x.batchNo})`,
       style: `${x.recipe.style.name} - ${x.recipe.style.category} (${x.recipe.style.categoryNumber}${x.recipe.style.styleLetter})`,
       description: x.tasteNotes ?? '',
       brewDate: getDateString(x.brewDate),
       packageDate: getDateString(x.bottlingDate),
       calories: Math.floor(x.recipe.nutrition.calories.total * 3.5),
-      type: x.status === 'Fermenting' || x.status === 'Brewing' || x.status === 'Planning' ? 'fermenting' : 'packaged',
+      type: 'packaged',
     }));
 
     const storedBeers = await db
       .collection('beer')
       .get()
       .then((x) => x.docs.map((d) => ({ id: d.id, ...d.data() } as Beer)));
+    
+    for (const storedBeer of storedBeers) {
+      const brewfatherBeer = beers.find(x => x.brewfatherId === storedBeer.brewfatherId);
+      if (brewfatherBeer) {
+        continue;
+      }
+
+      console.log(`Removing beer ${storedBeer.name} - ${storedBeer.style} from Firestore`);
+      await db.collection('beer').doc(storedBeer.id ?? '').delete();
+    }
 
     for (const brewfatherBeer of beers) {
       const stored = storedBeers.find((x) => x.brewfatherId === brewfatherBeer.brewfatherId);
@@ -71,7 +81,7 @@ const getBrewfatherBatchDetails = async (id: string): Promise<BrewfatherBatchDet
   await request(`https://api.brewfather.app/v2/batches/${id}`);
 
 const getBrewfatherBatchList = async (): Promise<BrewfatherBatch[]> =>
-  await request('https://api.brewfather.app/v2/batches?order_by=brewDate&order_by_direction=desc');
+  await request('https://api.brewfather.app/v2/batches?order_by=brewDate&order_by_direction=desc&status=Completed');
 
 const request = async <T>(path: string): Promise<T> => {
   const response = await fetch(path, {
