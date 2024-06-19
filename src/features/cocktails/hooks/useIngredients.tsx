1cef3bc5baa4a2da9a8109324371dfa6385acbdf
Brandon Cox <cox5529@gmail.com>
2024-06-19 19:53:19 -0400

    Fix build

--- src/features/cocktails/hooks/useIngredients.tsx
+++ src/features/cocktails/hooks/useIngredients.tsx
@@ -6,15 +6,16 @@ import { useCollection } from 'react-firebase-hooks/firestore';
 import { firestore } from '../../../firebase';
 import { Ingredient } from '../models/ingredient';
 
-export function useIngredients(ids: string[]): [Ingredient[], boolean] {
+export function useIngredients(ids?: string[]): [Ingredient[], boolean] {
+  
   const ingredientCollection = collection(firestore, 'ingredients') as CollectionReference<Ingredient>;
-  const ingredientQuery = query(ingredientCollection, where('id', 'in', ids));
+  const ingredientQuery = ids ? query(ingredientCollection, where('id', 'in', ids)) : ingredientCollection;
 
   const [ingredients, isLoading] = useCollection<Ingredient>(ingredientQuery);
 
   return useMemo(() => {
     const data = ingredients?.docs.map((x) => ({ ...x.data(), id: x.id })) ?? [];
-    const result = ids.map((x) => data.find((y) => y.id === x) as Ingredient).filter((x) => !!x);
+    const result = ids ? ids.map((x) => data.find((y) => y.id === x) as Ingredient).filter((x) => !!x): data;
 
     return [result, !isLoading];
   }, [ingredients?.docs, isLoading, ids]);
